6a0a1eff0b167f2775264fc5de6d2824b34d59bc
DagmarXx <155630154+DagmarXx@users.noreply.github.com>
2024-08-09 14:06:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const formulario = document.getElementById('contac-from');
 
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch('https://sheet.best/api/sheets/7029e30c-7be4-4511-8f2e-03b832aacee3',{
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "Nombre": "Ejemplo de nombre",
+      "Apellido":"Ejemplo de Apellido",
+      "Correo":"Ejemplo de Correo",
+      "Numero":"Ejemplo de Numero",
+      "WhatsApp":"Ejemplo de WhatsApp",
+      "Telegram":"Ejemplo de Telegram",
+      "Ninguno":"Ejemplo de Ninguno",
+      "Acepto":"Ejemplo de Acepto" 
+    })
+  });
+});
